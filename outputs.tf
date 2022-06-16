e9db2514c8ee15730250957f998985092dd440e3
jvallexm <jvallexm@gmail.com>
2022-06-16 10:16:43 -0400

    Update input

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,7 @@
 output "acls" {
   description = "List of Network ACL names and ids"
   value = [
-    for network_acl in ibm_is_network_acl.network_acl :
+    for network_acl in ibm_is_network_acl.acl :
     {
       id   = network_acl.id
       name = network_acl.name
